345b3579789b8ee3630f005443cc8f1f2833c412
Steve Northover <snorthov@ca.ibm.com>
2015-12-18 20:56:05 +0000

    test

--- index.js
+++ index.js
@@ -13,11 +13,31 @@ express = require('express');
 ghost = require('./core');
 errors = require('./core/server/errors');
 
+if (false) {
+	var app = express();
+	app.get('/', function (req, res) {
+	  res.end('It Works!! Path Hit: ' + req.url);
+	});
+	var host = (process.env.VCAP_APP_HOST || 'localhost');
+	var port = (process.env.VCAP_APP_PORT || 2368);
+	app.listen(port, host);
+	return;
+}
+
 // Create our parent express app instance.
 parentApp = express();
 
 // Call Ghost to get an instance of GhostServer
 ghost().then(function (ghostServer) {
+
+	parentApp.get('/', function (req, res) {
+	  res.end('It Works!! Path Hit 2: ' + req.url);
+	});
+	var host = (process.env.VCAP_APP_HOST || 'localhost');
+	var port = (process.env.VCAP_APP_PORT || 2368);
+	parentApp.listen(ghostServer.config.server.port, ghostServer.config.server.host);
+	return;
+
     // Mount our Ghost instance on our desired subdirectory path if it exists.
     parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
 
